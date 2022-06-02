da80d3b53ce581e9bba1fe067878ec62835a180d
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-06-02 15:39:35 -0300

    build(nodemailer): added nodemailer and start sending emails from every feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from "express"
+import nodemailer from "nodemailer"
+import { prisma } from "./prisma"
 
 const app = express()
 
@@ -10,8 +12,42 @@ const app = express()
 // PATCH = Atualizar uma informação única de uma entidade
 // DELETE = Deletar uma informação
 
-app.post("/feedbacks", (req, res) => {
-  return res.send("Hello World")
+app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b193d03e25afb4",
+    pass: "4c5d1b9d779c35"
+  }
+})
+
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Zaiko Xander <alexdaniel.lima.a@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      "<div style='font-family: sans-serif; font-size: 16px; color: #111'>",
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      "</div>"
+    ].join("\n")
+  })
+
+  // status http, 201 = sinaliza que algo foi criado
+  return res.status(201).json({ data: feedback })
 })
 
 app.listen(3333, () => {
