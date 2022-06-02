c5d5b939abc0bf177dc27b42191d57e81e4c65bb
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-06-02 18:02:55 -0300

    feat: applying the concept of Dependency Inversion

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,8 @@
 import express from "express"
 import nodemailer from "nodemailer"
 import { prisma } from "./prisma"
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository"
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case"
 
 export const routes = express.Router()
 
@@ -16,26 +18,27 @@ const transport = nodemailer.createTransport({
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body
 
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository)
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot
   })
 
-  await transport.sendMail({
-    from: "Equipe Feedget <oi@feedget.com>",
-    to: "Zaiko Xander <alexdaniel.lima.a@gmail.com>",
-    subject: "Novo feedback",
-    html: [
-      "<div style='font-family: sans-serif; font-size: 16px; color: #111'>",
-      `<p>Tipo do feedback: ${type}</p>`,
-      `<p>Comentário: ${comment}</p>`,
-      "</div>"
-    ].join("\n")
-  })
+  // await transport.sendMail({
+  //   from: "Equipe Feedget <oi@feedget.com>",
+  //   to: "Zaiko Xander <alexdaniel.lima.a@gmail.com>",
+  //   subject: "Novo feedback",
+  //   html: [
+  //     "<div style='font-family: sans-serif; font-size: 16px; color: #111'>",
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     "</div>"
+  //   ].join("\n")
+  // })
 
   // status http, 201 = sinaliza que algo foi criado
-  return res.status(201).json({ data: feedback })
+  return res.status(201).send()
 })
