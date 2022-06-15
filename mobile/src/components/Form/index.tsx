affc0e000ebaaf1faae4df7531812c2d71443ef5
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2022-06-15 19:49:59 -0300

    feat(mail): added the screenshot to the return of the feedback mail

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,17 +1,18 @@
+import { useState } from "react"
 import { Image, Text, TouchableOpacity, View } from "react-native"
 import { BottomSheetTextInput } from "@gorhom/bottom-sheet"
-
 import { ArrowLeft } from "phosphor-react-native"
+import { captureScreen } from "react-native-view-shot"
+import * as FileSystem from "expo-file-system"
 
 import { FeedbackType } from "../Widget"
-import { feedbackTypes } from "../../utils/feedbackTypes"
+import { Button } from "../Button"
 import { ScreenshotButton } from "../ScreenshotButton"
 
 import { styles } from "./styles"
 import { theme } from "../../theme"
-import { Button } from "../Button"
-import { useState } from "react"
-import { captureScreen } from "react-native-view-shot"
+import { feedbackTypes } from "../../utils/feedbackTypes"
+import { api } from "../../libs/api"
 
 interface Props {
   feedbackType: FeedbackType
@@ -22,6 +23,7 @@ interface Props {
 export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props) {
   const [isSendingFeedback, setIsSendingFeedback] = useState(false)
   const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState("")
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
@@ -43,6 +45,20 @@ export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props
     }
 
     setIsSendingFeedback(true)
+    const screenshotBase64 = screenshot && await FileSystem.readAsStringAsync(screenshot, { encoding: "base64" })
+
+    try {
+      await api.post("/feedbacks", {
+        type: feedbackType,
+        screenshot: `data:image/png;base64, ${screenshotBase64}`,
+        comment
+      })
+
+      onFeedbackSent()
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -73,6 +89,7 @@ export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         placeholderTextColor={theme.colors.text_secondary}
         autoCorrect={false}
+        onChangeText={setComment}
       />
 
       <View style={styles.footer}>
